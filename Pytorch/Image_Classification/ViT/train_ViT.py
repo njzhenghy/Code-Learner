90e01bd6708269cb951f578de113f424388155d3
zhy <njzhenghy@aliyun.com>
2024-11-04 15:18:33 +0800

    Add argparse code

--- Pytorch/Image_Classification/ViT/train_ViT.py
+++ Pytorch/Image_Classification/ViT/train_ViT.py
@@ -1,27 +1,50 @@
+import argparse
 import torch
 import torch.optim as optim
 import torch.nn as nn
 from model import ViT
 from dataloader import train_loader_mnist, val_loader_mnist
 
+arg = argparse.ArgumentParser()
+arg.add_argument('--epochs', type=int, default=50, help='Number of epochs to train')
+arg.add_argument('--lr', type=float, default=0.01, help='Learning rate')
+arg.add_argument('-batch_size', type=int, default=16, help='Batch size')
+arg.add_argument('--seed', type=int, default=32, help='Random seed')
+arg.add_argument('--device', type=str, default='cuda', help='Device used for training & validation')
+arg.add_argument('--gpu', type=int, default=0, help='GPU ID')
+arg.add_argument('--img_size', type=int, default=28, help='Image size')
+arg.add_argument('--in_channels', type=int, default=3, help='Image channels')
+arg.add_argument('--num_classes', type=int, default=10, help='Number of classes')
+arg.add_argument('--patch_size', type=int, default=8, help='Patch size')
+arg.add_argument('--depth', type=int, default=6, help='Depth')
+arg.add_argument('--heads', type=int, default=4, help='Number of heads')
+arg.add_argument('--dim', type=int, default=8, help='ViT Dimension')
+arg.add_argument('--mlp_dim', type=int, default=16, help='MLP Dimension')
+args = arg.parse_args()
+
 model = ViT(
-    image_size=28,
-    patch_size=7,
-    num_classes=10,
-    dim=8,
-    depth=1,
-    heads=8,
-    mlp_dim=16,
+    image_size=args.img_size,
+    patch_size=args.patch_size,
+    num_classes=args.num_classes,
+    dim=args.dim,
+    depth=args.depth,
+    heads=args.heads,
+    mlp_dim=args.mlp_dim,
     dropout=0.1,
     emb_dropout=0.1
 )
 
-device = torch.device('mps' if torch.backends.mps.is_built() else 'cpu')
+if torch.cuda.is_available():
+    device = torch.device(f"{args.device}:{args.gpu}")
+elif torch.backends.mps.is_built():
+    device = torch.device('mps')
+else:
+    device = torch.device('cpu')
 print(device)
 model.to(device)
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=1e-4)
+optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
 
 # Training loop
