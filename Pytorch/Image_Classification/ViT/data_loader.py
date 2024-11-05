ab7354c7876743c9fecc2402bf3d92aee6c0b82a
zhy <njzhenghy@aliyun.com>
2024-11-05 23:28:47 +0800

    Change the code format

--- Pytorch/Image_Classification/ViT/data_loader.py
+++ Pytorch/Image_Classification/ViT/data_loader.py
@@ -5,6 +5,8 @@ from torch.utils.data import DataLoader
 from utils import data_transforms
 
 ssl._create_default_https_context = ssl._create_unverified_context
+
+
 def data_loader(args):
     if args.dataset == 'cifar10':
         num_classes = 10
@@ -23,12 +25,12 @@ def data_loader(args):
 
         valid_dataset = datasets.MNIST(root=args.data_root, train=False, transform=valid_transform, download=True)
         valid_loader = DataLoader(valid_dataset, batch_size=args.batch_size, shuffle=True)
-    
+
     elif args.dataset == 'tiny-imagenet':
         pass
 
     else:
         print('dataset error')
         sys.exit(1)
-    
+
     return num_classes, train_loader, valid_loader
