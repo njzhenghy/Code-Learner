a822d1ebd603ae52b83349c54bf6ef9b041e0fe0
zhy <njzhenghy@aliyun.com>
2024-11-17 15:30:15 +0800

    Added MoCo comparative learning code

---
+++ Pytorch/Contrast_Learning/MoCo/loader.py
@@ -0,0 +1,25 @@
+import random
+from PIL import ImageFilter
+
+class TwoCropsTransform:
+    """Take two random crops of one image as the query and key."""
+
+    def __init__(self, base_transform):
+        self.random_transform = random_transform
+
+    def __call__(self, x):
+        q = self.random_transform(x)
+        k = self.random_transform(x)
+        return [q, k]
+
+
+class GaussianBlur:
+    """Gaussian blur augmentation in SimCLR https://arxiv.org/abs/2002.05709"""
+
+    def __init__(self, sigma=[0.1, 2.0]):
+        self.sigma = sigma
+
+    def __call__(self, x):
+        sigma = random.uniform(self.sigma[0], self.sigma[1])
+        x = x.filter(ImageFilter.GaussianBlur(radius=sigma))
+        return x
