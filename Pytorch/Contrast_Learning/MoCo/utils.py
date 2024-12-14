96a1fa21e49c1e719c4a220160c5d1d433667f51
zhy <49585078+njzhenghy@users.noreply.github.com>
2024-12-14 23:49:05 +0800

    deleted the redundant function

--- Pytorch/Contrast_Learning/MoCo/utils.py
+++ Pytorch/Contrast_Learning/MoCo/utils.py
@@ -26,19 +26,6 @@ class AverageMeter():
         self.avg = self.sum / self.cnt
 
 
-def adjust_learning_rate(optimizer, epoch, args):
-    """Decay the learning rate based on schedule"""
-    lr = args.lr
-    if args.cos:  # cosine lr schedule
-        lr *= 0.5 * (1.0 + math.cos(math.pi * epoch / args.epochs))
-    else:  # stepwise lr schedule
-        for milestone in args.schedule:
-            lr *= 0.1 if epoch >= milestone else 1.0
-    for param_group in optimizer.param_groups:
-        param_group["lr"] = lr
-    
-    return lr
-
 
 def accuracy(output, target, topk=(1,)):
     """Computes the accuracy over the k top predictions for the specified values of k"""
